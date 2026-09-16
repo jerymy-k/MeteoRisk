9404d733ec8a2475cfea96b728bbae22763621ff
jerymy-k <karimimoha0@gmail.com>
2026-09-16 17:27:29 +0100

    feat: add project config (Open-Meteo API URL and daily variables)

---
+++ config.py
@@ -0,0 +1,2 @@
+OPEN_METEO_URL = "https://api.open-meteo.com/v1/forecast"
+DAILY_VARS = "temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,weathercode"
